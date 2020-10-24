03e60f826cc40ddf74cb4fabc9d80232f611840b
LUIS FELIPE TEJADA PADILLA <lftejadap@uqvirtual.edu.co>
2020-10-23 23:15:23 -0500

    config whitenoise and requirements

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -60,10 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoapp.urls'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 TEMPLATES = [
