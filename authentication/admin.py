dbe9c6cd516348c1bb18a0dd8b429a04b168b433
Juan Manuel Roa Mejia <juanmanuelrm2009@gmail.com>
2020-10-27 10:56:14 -0500

    Modelos BD listos

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,9 +1,15 @@
-# -*- encoding: utf-8 -*-
-"""
-License: MIT
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Project)
+
+admin.site.register(Requirement)
+admin.site.register(Iteration)
+admin.site.register(IterationTask)
+admin.site.register(IterationTaskHistorial)
+
+admin.site.register(TaskProxy)
+admin.site.register(PlanningEntry)
+#admin.site.register(PlanningPeriod)
+admin.site.register(TeamMember)
