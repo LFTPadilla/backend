32631730c3126b993ad9af8260665eb015ae4080
Juan Manuel Roa Mejia <49876718+dot-roa@users.noreply.github.com>
2020-11-12 12:22:44 -0500

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -7,3 +7,7 @@ Copyright (c) 2019 - present AppSeed.us
 from django.test import TestCase
 
 # Create your tests here.
+
+class NumberTestCase(TestCase):
+  def simple_test(self):    
+    self.assertEqual(4,4) 
