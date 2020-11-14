2a9793c9be9ef9018820591ddb8e7edb68d82948
LUIS FELIPE TEJADA PADILLA <lftejadap@uqvirtual.edu.co>
2020-11-14 09:32:53 -0500

    test mockup

--- app/tests.py
+++ app/tests.py
@@ -6,11 +6,20 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.test import TestCase    #Testear cosas de la BD
 import unittest                     #Testear normal
-
+import requests_mock
 # Create your tests here.
 
 class NumberTestCase(TestCase):
 
   #Ojo, Deben llevar test_ para que funcione      
-  def test_number_sum(self):    
-    self.assertEqual("SI", "SI")
+  def test_get_updated_issues_one_page(self):
+    with open("app/issues_one_page.json", "r") as issues_file:
+        mock_response = issues_file.read()
+ 
+    expected_result = {'Success':'false',"User":'null'}
+ 
+    with requests_mock.Mocker() as m:
+        m.post('/auth/login', json={'login':'felipe','password':'12345678'})
+        print(m.)
+        #response = jiratimereport.get_updated_issues("https://jira_url", "user_name", "api_token",  "MYB", "2020-01-10", "2020-01-20", "")
+    self.assertEqual(expected_result, response)
